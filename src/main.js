15f6da17d29362928b023574bb61891b85f1dfac
TyshchenkoYulia <145122847+TyshchenkoYulia@users.noreply.github.com>
2024-02-24 23:18:48 +0200

    add markup

---
+++ src/main.js
@@ -0,0 +1,33 @@
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// ====================================================================
+
+import errorIcon from "../src/img/error-icon.svg";
+
+// const lightbox = new SimpleLightbox('.gallery a',
+//                                  { captionType: 'attr',
+//                                    captionsData: 'alt',
+//                                    captionDelay: 250,
+//                                  });
+
+
+// ===================================================================
+
+//  iziToast.show({
+//                 titleColor: '#fff',
+//                 titleSize: '16px',
+//                 message: 'Sorry, there are no images matching your search query. Please try again!',
+//                 messageColor: '#fff',
+//                 messageSize: '16px',
+//                 iconUrl: errorIcon,
+//                 position: 'topRight',
+//                 backgroundColor: '#ef4040',
+//                 close: true,
+//                 maxWidth: '432px',
+                
+//             });
